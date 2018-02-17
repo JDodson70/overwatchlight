5cf99024971bf31d50c6c336804215560f3d2e78
ben <ben@voyagermark.com>
2018-02-17 01:24:00 -0500

    dva css

---
+++ src/heroes/tracer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './tracer.css';
+
+class Tracer extends Component {
+  render() {
+    return <h1>Tracer Page</h1>;
+  }
+}
+
+export default Tracer;
